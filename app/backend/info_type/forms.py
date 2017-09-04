0a952ff260603a23b1bb4f55c5d74c251e7b8072
huo_mac <240516816@qq.com>
2017-09-04 17:08:25 +0800

    优化信息类模块

--- app/backend/info_type/forms.py
+++ app/backend/info_type/forms.py
@@ -12,7 +12,3 @@ class InfoTypeForm(FlaskForm):
     name = StringField(u'名称', validators=[DataRequired(), Length(1, 30)])
     submit = SubmitField(u'提交')
     return_link = LinkField(u'返回', '/backend/info_type/index')
-
-    def validate_name(self, field):
-        if InfoType.query.filter_by(name=field.data).first():
-            raise ValidationError(u'名称已存在.')
--- app/backend/info_type/views.py
+++ app/backend/info_type/views.py
@@ -23,22 +23,27 @@ def new():
     form = InfoTypeForm()
     if request.method == "POST":
         if form.validate_on_submit():
-            form_data = dict(name=form.name.data,
-                             user_id=current_user.id
-                             )
+            exist_info_type = InfoType.query.filter_by(name=form.name.data).first()
+            if exist_info_type:
+                flash(u'名称{}已存在!'.format(form.name.data), 'danger')
+                return _render('form', locals())
+            else:
+                form_data = dict(name=form.name.data,
+                                 user_id=current_user.id
+                                 )
 
-            new_object = InfoType(**form_data)
-            db.session.add(new_object)
-            try:
-                db.session.commit()
-                flash(u'信息类{}添加成功!'.format(form_data['name']))
-                return redirect(url_for('.index'))
-            except Exception, e:
-                flash(u'信息类{}保存失败, 请联系管理员!'.format(form_data['name']), 'danger')
-                print e.message
-                db.session.rollback()
+                new_object = InfoType(**form_data)
+                db.session.add(new_object)
+                try:
+                    db.session.commit()
+                    flash(u'信息类{}添加成功!'.format(form_data['name']))
+                    return redirect(url_for('.index'))
+                except Exception, e:
+                    flash(u'信息类{}保存失败, 请联系管理员!'.format(form_data['name']), 'danger')
+                    print e.message
+                    db.session.rollback()
         else:
-            flash(u'提交失败!', 'danger')
+            flash(u'表单验证失败!', 'danger')
     return _render('form', locals())
 
 
@@ -51,11 +56,16 @@ def edit(id):
     print form.validate_on_submit()
     if request.method == "POST":
         if form.validate_on_submit():
-            form.populate_obj(info_type)
-            flash(u'信息类{}编辑成功!'.format(info_type.name))
-            return redirect(url_for(".index"))
+            exist_info_type = InfoType.query.filter_by(name=form.name.data).first()
+            if exist_info_type and exist_info_type.id != info_type.id:
+                flash(u'名称{}已存在!'.format(form.name.data), 'danger')
+                return _render('form', locals())
+            else:
+                form.populate_obj(info_type)
+                flash(u'信息类{}编辑成功!'.format(info_type.name))
+                return redirect(url_for(".index"))
         else:
-            flash(u'提交失败!', 'danger')
+            flash(u'表单验证失败!', 'danger')
     return _render("form", locals())
 
 
